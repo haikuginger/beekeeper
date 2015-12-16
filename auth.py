f577b02c72434465460471d1828bfda4e3479dd3
Jesse Shapiro <jesse@bedrockdata.com>
2015-12-16 14:33:27 -0500

    Consolidating utilities module

--- auth.py
+++
@@ -1,6 +0,0 @@
-import base64
-
-def httpbasicauth(username, password):
-        authinfo = base64.b64encode("{}:{}".format(username,password).encode('utf-8'))
-        authinfo = "Basic {}".format(authinfo.decode('utf-8'))
-        return authinfo
--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 import json
 import urllib.request
+import base64
 
 def download_as_json(url):
     return json.loads(urllib.request.urlopen(url).read().decode('utf-8'))
+
+def httpbasicauth(username, password):
+    authinfo = base64.b64encode("{}:{}".format(username,password).encode('utf-8'))
+    authinfo = "Basic {}".format(authinfo.decode('utf-8'))
+    return authinfo
