1916fee924fc3b70dda1ed273f936260454c3c57
Jesse Shapiro <jesse@bedrockdata.com>
2015-12-28 09:48:29 -0600

    Adding docstrings and cleaning up logic

--- beekeeper.py
+++ beekeeper.py
@@ -1,3 +1,8 @@
+"""
+This module contains all the beekeeper classes that are used on the front end
+to directly interface between the developer and the remote API.
+"""
+
 import copy
 from variables import Variables
 from hive import Hive
@@ -5,6 +10,10 @@ from parsers import decode
 from utils import request
 
 class Endpoint:
+
+    """
+    Contains the settings for an endpoint, as well as a backref to the API.
+    """
 
     def __init__(self, parent, path, **kwargs):
         self.parent = parent
@@ -14,17 +23,30 @@ class Endpoint:
         self.mimetype = kwargs.get('mimetype', None)
 
     def variables(self):
+        """
+        Get API-level variables, and add in Endpoint-level variables
+        """
         return self.parent.variables().add(**self.vars)
 
     def url(self):
+        """
+        Combine the API-level root URL with the Endpoint's path
+        """
         return self.parent.root + self.path
 
     def new_action(self, method='GET', **kwargs):
+        """
+        Create a new Action linked to this endpoint with the given args
+        """
         if method not in self.methods:
             raise TypeError("{} not in valid method(s): {}.".format(method, self.methods))
         return Action(self, method, **kwargs)
 
     def format(self):
+        """
+        Get the Endpoint-level mimetype, deferring to the API level settings
+        if the Endpoint object doesn't have a value
+        """
         if self.mimetype:
             return self.mimetype
         else:
@@ -32,19 +54,46 @@ class Endpoint:
 
 class APIObject:
 
+    """
+    Holds Action objects in the appropriate namespace, and provides a __getitem__
+    dundermethod so that we can subscript by object ID when such exists.
+    """
+
     def __init__(self, parent, actions, **kwargs):
         self.description = kwargs.get('description', None)
-        self.actions = actions
         self.id_variable = kwargs.get('id_variable', None)
-        for name, action in self.actions.items():
-            setattr(self, name, parent.new_action(**action).execute)
+        self.key_action = kwargs.get('keyed_action', 'get')
+        self.actions = {}
+        for name, action in actions.items():
+            self.add_action(name, parent, action)
 
     def __getitem__(self, key):
-        if "get" in self.actions and self.id_variable:
-            return self.get(**{self.id_variable:key})
+        """
+        Allows us to subscript, dictionary-style, on the object if we
+        know what the object's unique key is.
+        """
+        if 'get' in self.actions and self.id_variable:
+            return getattr(self, self.key_action)(**{self.id_variable:key})
         raise TypeError("Object cannot be addressed by ID")
 
+    def defined_actions(self):
+        """
+        Get a list of the available Actions on the APIObject
+        """
+        return [x for x, y in self.actions.items()]
+
+    def add_action(self, name, parent, action):
+        """
+        Add a single Action to the APIObject
+        """
+        self.actions[name] = action
+        setattr(self, name, parent.new_action(**action).execute)
+
 class Action:
+
+    """
+    Holds action-level variables and provides the .execute() method
+    """
 
     def __init__(self, endpoint, method, **kwargs):
         self.endpoint = endpoint
@@ -54,19 +103,36 @@ class Action:
         self.url = endpoint.url
 
     def variables(self):
+        """
+        Grab all the variables from the Endpoint and higher, and
+        add the ones that exist on the Action level
+        """
         return self.endpoint.variables().add(**self.vars)
 
     def execute(self, *args, **kwargs):
+        """
+        Fill all variables from *args and **kwargs, build the request,
+        send it, and parse the response appropriately as determined by
+        the MIME type defined in the hive.
+        """
         variables = self.variables().fill(*args, **kwargs)
-        return decode(request(**variables.render(self)),self.format(direction='returns'))
+        return decode(request(**variables.render(self)), self.format(direction='returns'))
 
     def format(self, direction='both'):
+        """
+        Get the local directional MIME type; if it doesn't exist, defer
+        to the Endpoint-level MIME type.
+        """
         if self.mimetype and direction in self.mimetype:
             return self.mimetype[direction]
         else:
             return self.endpoint.format()
 
 class API:
+
+    """
+    Holds global-level settings and provides initialization methods
+    """
 
     def __init__(self, root, variables, mimetype, *args, **kwargs):
         self.settings = Variables(**variables).fill(*args, **kwargs)
@@ -76,6 +142,11 @@ class API:
 
     @classmethod
     def from_hive(cls, hive, *args, **kwargs):
+        """
+        Initialize the API itself, then in order, add the hive-defined
+        Endpoints, and the APIObjects and Actions that reference those
+        Endpoints.
+        """
         this_api = cls(hive['root'], hive['variables'], hive['mimetype'], *args, **kwargs)
         for name, endpoint in hive['endpoints'].items():
             this_api.add_endpoint(name, **endpoint)
@@ -85,23 +156,48 @@ class API:
 
     @classmethod
     def from_hive_file(cls, fname, *args, version=None, **kwargs):
+        """
+        Open a local JSON hive file and initialize from the hive contained
+        in that file, paying attention to the Version keyword argument.
+        """
         return cls.from_hive(Hive.from_file(fname, version), *args, **kwargs)
 
     @classmethod
     def from_remote_hive(cls, url, *args, version=None, **kwargs):
+        """
+        Download a JSON hive file from a URL, and initialize from it,
+        paying attention to the Version keyword argument.
+        """
         return cls.from_hive(Hive.from_url(url, version), *args, **kwargs)
 
     def variables(self):
+        """
+        Return a copy of the API-level variables
+        """
         return copy.deepcopy(self.settings)
 
     def add_endpoint(self, name, **kwargs):
+        """
+        Add an endpoint with the given name to the API
+        """
         self.endpoints[name] = Endpoint(self, **kwargs)
 
     def add_object(self, name, obj):
+        """
+        Initialize an APIObject with the given name and make it available
+        using dot notation from the top-level namespace
+        """
         setattr(self, name, APIObject(self, **obj))
 
     def new_action(self, endpoint, **kwargs):
+        """
+        Initialize a new Action linked to the named Endpoint that's
+        a member of the API
+        """
         return self.endpoints[endpoint].new_action(**kwargs)
 
     def format(self):
+        """
+        Provide the API-level MIME type
+        """
         return self.mimetype
