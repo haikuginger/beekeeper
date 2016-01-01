d327b8a000141b3426d338ab91ed850649edd802
Jesse Shapiro <jesse@bedrockdata.com>
2016-01-01 17:45:04 -0600

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "beekeeper",
+    version = "0.1a1",
+    packages = find_packages(),
+    author = "Jesse Shapiro",
+    author_email = "jesse@bedrockdata.com",
+    keywords = "REST API web client wrapper",
+    url = "https://github.com/haikuginger/beekeeper",
+)
