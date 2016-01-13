2b10696fb5a4a2a42ba0ff7fee698bd8e3480d3a
Jesse Shapiro <jesse@bedrockdata.com>
2016-01-13 14:36:17 -0500

    Adding PyPI classifiers

--- setup.py
+++ setup.py
@@ -7,4 +7,22 @@ setup(
     author_email = "jesse@bedrockdata.com",
     keywords = "REST API web client wrapper",
     url = "https://github.com/haikuginger/beekeeper",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"
+    ]
 )
