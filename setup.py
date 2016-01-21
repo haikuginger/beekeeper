2a52573ee666058dad89af5d8e9a37bc0deb29a1
Jesse Shapiro <jesse@bedrockdata.com>
2016-01-20 19:37:55 -0500

    Marking as 0.5 in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "beekeeper",
-    version = "0.4",
+    version = "0.5",
     packages = ['beekeeper'],
     author = "Jesse Shapiro",
     author_email = "jesse@bedrockdata.com",
