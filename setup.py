629a80e7e7f74de3f384db1aeba828bc54a6fec2
Jesse Shapiro <jesse@bedrockdata.com>
2016-01-23 14:16:31 -0500

    Updating to 0.6.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "beekeeper",
-    version = "0.6",
+    version = "0.6.1",
     packages = ['beekeeper'],
     author = "Jesse Shapiro",
     author_email = "jesse@bedrockdata.com",
