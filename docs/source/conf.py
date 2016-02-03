729599180d45b49e254ac5ac8dc6e6116464c886
Jesse Shapiro <jesse@bedrockdata.com>
2016-02-03 15:05:08 -0500

    Adding authors.txt, updating docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'beekeeper'
-copyright = '2016, Jesse Shapiro'
+copyright = '2015 - 2016, Jesse Shapiro'
 author = 'Jesse Shapiro'
 
 # The version info for the project you're documenting, acts as replacement for
