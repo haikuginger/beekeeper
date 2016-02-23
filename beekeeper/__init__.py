7dac71ff7e37a5d140afb9be866a292814e5426a
Jesse Shapiro <jesse@bedrockdata.com>
2016-02-23 16:03:59 -0500

    Refactoring final variable handling to use callbacks instead of deep returns

--- beekeeper/__init__.py
+++ beekeeper/__init__.py
@@ -5,4 +5,5 @@ Importing certain classes into the global namespace.
 from beekeeper.api import API
 from beekeeper.hive import Hive
 from beekeeper.data_handlers import add_data_handler
-from beekeeper.variable_handlers import add_variable_handler, render as render_variables
+from beekeeper.variable_handlers import add_variable_handler, set_content_type
+from beekeeper.variable_handlers import render as render_variables
--- beekeeper/variable_handlers.py
+++ beekeeper/variable_handlers.py
@@ -2,9 +2,9 @@
 This module provides methods that parse variables into
 one of the three base level variable types. We map the
 variable type names to methods that can handle them. Each
-method should either return an iterable (either natively
-or by returning an executed generator), or should yield
-single objects (in other words, be a generator).
+method consumes a Request object 'rq' as the first positional
+argument, and does its work by calling one of the variable-setting
+methods on rq.
 """
 
 from __future__ import absolute_import, division
@@ -16,19 +16,18 @@ from base64 import b64encode
 
 from beekeeper.data_handlers import encode
 
-def content_type_header(mimetype):
-    return {'name': 'Content-Type', 'type': 'header', 'value': mimetype}
+def identity(**values):
+    return {name: val['value'] for name, val in values.items()}
 
-def identity(var_type, **values):
-    for name, value in values.items():
-        yield dict(type=var_type, name=name, value=value['value'])
+def set_content_type(rq, mimetype):
+    rq.set_headers(**{'Content-Type': mimetype})
 
-def render_data(**data):
+def render_data(rq, **data):
     for _, val in data.items():
-        yield content_type_header(val['mimetype'])
-        yield {'type': 'data', 'value': encode(val['value'], val['mimetype'])}
+        rq.set_headers(**content_type_header(val['mimetype']))
+        rq.set_data(encode(val['value'], val['mimetype']))
 
-def http_form(**values):
+def http_form(rq, **values):
     form = {
         'x': {
             'mimetype': 'application/x-www-form-urlencoded',
@@ -37,28 +36,28 @@ def http_form(**values):
             }
         }
     }
-    return render('data', **form)
+    render(rq, 'data', **form)
 
-def basic_auth(**values):
+def basic_auth(rq, **values):
     username = values.get('username', {}).get('value', '')
     password = values.get('password', {}).get('value', '')
     authinfo = b64encode("{}:{}".format(username, password).encode('utf-8'))
     authinfo = 'Basic {}'.format(authinfo.decode('utf-8'))
-    return render('header', Authorization={'value': authinfo})
+    rq.set_headers(Authorization=authinfo)
 
-def bearer(**values):
+def bearer(rq, **values):
     if len(values) > 1:
         raise Exception('Only one bearer token allowed')
     else:
         for _, token in values.items():
             text = 'Bearer {}'.format(token['value'])
-            return render('header', Authorization={'value': text})
+            rq.set_headers(Authorization=text)
 
-def cookies(**values):
-    cookie = {'value':'; '.join([value['value'] for _, value in values.items()])}
-    return render('header', Cookie=cookie)
+def cookies(rq, **values):
+    cookie = '; '.join([value['value'] for _, value in values.items()])
+    rq.set_headers(Cookie=cookie)
 
-def multipart(**values):
+def multipart(rq, **values):
     frame = '\n--{}\nContent-Disposition: form-data; name="{}"'
     boundary = uuid4().hex
     files = [name for name, data in values.items() if data.get('mimetype', False)]
@@ -75,15 +74,25 @@ def multipart(**values):
             args = (boundary, name)
         output += this_frame.format(*args).encode('ascii') + this_data
     output += '\n--{}--'.format(boundary).encode('ascii')
-    yield {'type': 'data', 'value': output}
-    yield content_type_header('multipart/form-data; boundary={}'.format(boundary))
+    rq.set_data(output)
+    header = 'multipart/form-data; boundary={}'.format(boundary)
+    set_content_type(rq, header)
+
+def header(rq, **values):
+    rq.set_headers(**identity(**values))
+
+def replacement(rq, **values):
+    rq.set_url_replacements(**identity(**values))
+
+def url_param(rq, **values):
+    rq.set_url_params(**identity(**values))
 
 VARIABLE_TYPES = {
     'http_form': http_form,
-    'header': partial(identity, 'header'),
+    'header': header,
     'data': render_data,
-    'url_replacement': partial(identity, 'url_replacement'),
-    'url_param': partial(identity, 'url_param'),
+    'url_replacement': replacement,
+    'url_param': url_param,
     'http_basic_auth': basic_auth,
     'cookie': cookies,
     'multipart': multipart,
@@ -93,7 +102,7 @@ VARIABLE_TYPES = {
 def add_variable_handler(var_type, handler):
     VARIABLE_TYPES[var_type] = handler
 
-def render(var_type, **values):
+def render(rq, var_type, **values):
     if var_type in VARIABLE_TYPES:
         variables = {val.pop('name', name): val for name, val in values.items()}
-        return VARIABLE_TYPES[var_type](**variables)
+        VARIABLE_TYPES[var_type](rq, **variables)
